7049d95c2e3c08569f77475a57942d4919d0469f
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-01-08 21:31:58 +0300

    task 07 completed with debug check

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+# Задача 7. Года
+
+start_period = int(input('Введите первый год: '))
+stop_period = int(input('Введите второй год: '))
+
+def tripple_num(year):
+  symbol1 = year // 1000
+  symbol2 = year // 100 % 10
+  symbol3 = year // 10 % 10
+  symbol4 = year % 10
+  if (symbol1 == symbol2 == symbol3) or (symbol1 == symbol3 == symbol4) or (symbol2 == symbol3 == symbol4) or (symbol2 == symbol1 == symbol4):
+    print(year)
+
+
+print('Года от', start_period, 'до', stop_period, 'с тремя одинаковыми цифрами: ')
+
+for year in range(start_period, stop_period + 1):
+  tripple_num(year)
+
+
