04e9f2f7fb98becfaddff0ec681f685bf5369be2
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-01-08 18:06:13 +0300

    task 03 debug check added and function naming changed

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -2,22 +2,22 @@
 
 number = int(input('Введите число: '))
 
-def summ_of_num(number):
+def summ_of_numbers(number):
     summ_of_num = 0
     while number > 0:
         summ_of_num += number % 10
         number //= 10
     return summ_of_num
 
-def amount_of_num(number):
+def amount_of_numbers(number):
     amount_of_num = 0
     while number > 0:
         amount_of_num += 1
         number //= 10
     return amount_of_num
 
-distinction = abs(summ_of_num(number) - amount_of_num(number))
+distinction = abs(summ_of_numbers(number) - amount_of_numbers(number))
 
-print('Сумма чисел: ', summ_of_num(number))
-print('Количество чисел: ', amount_of_num(number))
+print('Сумма чисел: ', summ_of_numbers(number))
+print('Количество чисел: ', amount_of_numbers(number))
 print('Разность суммы и количества цифр: ', distinction)
