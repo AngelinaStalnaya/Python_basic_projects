5c38a51e8709da75f2acc5ffe7dd0f5413fae2fe
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-01-08 16:31:37 +0300

    решение готово

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+# Задача 5. Наименьший делитель
+
+def least_divisor(number):
+    divisor_middle = 0
+    divisor_least = 0
+    for divisor in range(number, 1, -1):
+        if number % divisor == 0:
+           divisor_middle = divisor
+           if divisor_middle > 1:
+               divisor_least = divisor_middle
+    return divisor_least
+
+number = int(input('Введите число: '))
+if number <= 1:
+    print('Некорректный ввод, попробуйте снова.')
+else:
+    print('Наименьший общий делитель, оличный от единицы : ', least_divisor(number))
