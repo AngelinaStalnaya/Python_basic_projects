e487cc0ceb7e486be2c50cb26755c4149e6f0ec1
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-01-08 15:31:21 +0300

    решение готово

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,20 @@
-# TODO здесь писать код
+# Задача 4. Число наоборот 3
 
+number1 = float(input('Введите первое число: '))
+number2 = float(input('Введите второе число: '))
 
+def rev_num(number1):
+    new_num = ''
+    first_part = ''
+    second_part = ''
+    a_part, b_part = str(number1).split('.')
+    for symbol in a_part:
+        first_part = symbol + first_part
+    for symbol in b_part:
+        second_part = symbol + second_part
+    new_num = first_part + '.' + second_part
+    return float(new_num)
+
+print('Первое число наоборот: ', rev_num(number1))
+print('Второе число наоборот: ', rev_num(number2))
+print('Сумма чисел: ', rev_num(number1) + rev_num(number2))
