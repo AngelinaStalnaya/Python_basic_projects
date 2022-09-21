141596155220f8808667b873e0bd76758c507644
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-09-21 12:02:38 +0300

    homework 21, tasks 1 5,7 modified

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,18 +1,28 @@
-def calculating_math_func(data):
-    def factorial_num(data):
-        if data == 1:
-            return 1
-        else:
-            return data * factorial_num(data - 1)
-    answer = factorial_num(data)
-    def power_of_num(num, power):
-        if power == 1:
-            return num
-        else:
-            return num * power_of_num(num, power - 1)
-    answer_2 = answer / power_of_num(data, 3)
-    answer_3 = power_of_num(answer_2, 10)
-    return answer_3
+def factorial_num(num, my_dict=dict()):
+    if num in my_dict:
+        return my_dict[num]
+    if num == 1:
+        return 1
+    fact = num * factorial_num(num-1, my_dict)
+    my_dict[num] = fact
+    return fact
+
+def power(a, n):
+    if n == 1:
+        return a
+    return a * power(a, n - 1)
 
 
-print(calculating_math_func(5))
+def calculating_math_func(data, my_dict):
+    if data > 1:
+        result = factorial_num(data, my_dict)
+    else:
+        result = 1
+    result /= power(data, 3)
+    result = power(result, 10)
+    return result
+
+fact_dict = dict()
+
+
+print(calculating_math_func(7, fact_dict))
--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,44 +1,13 @@
-# summa = 0
-#
-# def advanced_sum(*args):
-#     for argument in args:
-#         if isinstance(argument, int):
-#             global summa
-#             summa += argument
-#         else:
-#             for i_arg in argument:
-#                 advanced_sum(i_arg)
-#     return summa
-#
-#
-# print('Ответ: ', advanced_sum([[1, 2, [3]], [1], 3]))
-# print('Ответ: ', advanced_sum(1, 2, 3, 4, 5))
-
-
-# *****************all right******
-summa = 0
 def summ(*args):
-    global summa
-    summa = 0
-    def advanced_sum(*args):
-        def unpack_list(argument, list=[]):
-            if isinstance(argument, int):
-                list.append(argument)
-                global summa
-                summa += argument
-            else:
-                for i_elem in argument:
-                    unpack_list(i_elem, list)
-            return summa
+    sum = 0
+    for argument in args:
+        if isinstance(argument, int):
+            sum += argument
+        else:
+            for i_elem in argument:
+                sum += summ(i_elem)
+    return sum
 
-        for argument in args:
-            if isinstance(argument, int):
-                global summa
-                summa += argument
-            else:
-                return summa + unpack_list(argument)
-        return summa
-    return advanced_sum(*args)
-# #
-# print('Ответ: ', summ([[1, 2, [3]], [1], 3]))
-# print('Ответ: ', summ(1, 2, 3, 4, 5))
+
+print('Ответ: ', summ([[1, 2, [3]], [1], 3]))
+print('Ответ: ', summ(1, 2, 3, 4, 5))
