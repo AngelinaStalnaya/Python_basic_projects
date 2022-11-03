2224d27d36ba6da9da699e86af00b0f59b495aae
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-11-03 19:15:05 +0300

    Homework module 26 added

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+from collections.abc import Iterable
+
+
+def QHofstadter(num_list: list) -> Iterable[int]:
+    """
+    Генератор поиска последующего числа и формирование последовательности
+    чисел Q Хофштадтера
+    :param num_list: список из двух чисел: 1 - начальное число последовательности,
+    2 - конечное число последовательности
+    :return: последовательность чисел
+    """
+    if num_list == [1, 2]:
+        print('Последовательность завершена.')
+        return
+
+    def function(number: int) -> int:
+        """
+        Генератор следующего числа последовательности
+        :param number: число, для которого необходимо найти значение
+        согласно формуле нахождения в последовательности
+        :return: значение числа в последовательности
+        """
+        if number < 3:
+            return 1
+        else:
+            result = function(number - function(number - 1)) + function(number - function(number - 2))
+            return result
+
+    for i in range(num_list[0], num_list[1] + 1):
+        yield function(i)
+
+
+my_function = QHofstadter([1, 10])
+for i_number in my_function:
+    print(i_number, end=' ')
--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+
+
+def find_py_files(path: dir) -> Iterable[int]:
+    """
+    Генератор подсчета строк кода в файлах расширения ".ру" в указанной директории
+    :param path: передаваемая директория для перебора содержащихся файлов
+    :return: None
+    """
+    for i_elem in os.listdir(path):
+        new_path = os.path.abspath(os.path.join(path, i_elem))
+        if os.path.isfile(new_path) and new_path.endswith('.py'):
+            with open(new_path, 'r', encoding='utf-8') as file_from:
+                all_lines = 0
+                for i_line in file_from:
+                    i_line.rstrip()
+                    if i_line != '\n' and not i_line.startswith('#') and not i_line.startswith('"'):
+                        all_lines += 1
+                yield all_lines
+        else:
+            find_py_files(new_path)
+
+
+my_path = r'C:\Users\Public\Python_Basic\Module24\07_cohabitation'
+my_files = find_py_files(my_path)
+all_lines_count = 0
+for i in my_files:
+    all_lines_count += i
+print('Общее количество строк кода в файлах расширения ".py" в указанной директории: ', all_lines_count)
