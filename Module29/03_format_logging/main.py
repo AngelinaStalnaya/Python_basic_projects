ed557b59ff2139f497ed8359f357a98ee8166c7c
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-11-22 11:55:09 +0300

    homework module 29 added.

--- Module29/03_format_logging/main.py
+++ Module29/03_format_logging/main.py
@@ -1,1 +1,67 @@
-# TODO здесь писать код
+from typing import Any
+from collections.abc import Callable
+import functools
+import time
+from datetime import datetime
+
+
+def log_methods(date_format: str) -> Callable:
+    def wrapper(cls) -> Callable:
+        for method in dir(cls):
+            if method.startswith("__") is False:
+                current_method = getattr(cls, method)
+                decorated_method = decorator(cls, current_method, date_format)
+                setattr(cls, method, decorated_method)
+        return cls
+    return wrapper
+
+
+def decorator(cls, func: Callable, date_format) -> Callable:
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs) -> Any:
+        new_format = date_format
+        for sym in new_format:
+            if sym.isalpha():
+                new_format = new_format.replace(sym, f'%{sym}')
+        start = time.time()
+        print(f'- Запускается "{cls.__name__}.{func.__name__}". '
+              f'Дата и время запуска: {datetime.now().strftime(new_format)}')
+        result = func(*args, **kwargs)
+        print(f'Тут метод {func.__name__} {"у наследника" if cls.__name__ == "B" else ""}')
+        print(f'- Завершение "{cls.__name__}.{func.__name__}", время работы = {round(time.time() - start, 3)}s')
+        return result
+    return wrapper
+
+
+@log_methods("b d Y - H:M:S")
+class A:
+    def test_sum_1(self) -> int:
+        print('test sum 1')
+        number = 100
+        result = 0
+        for _ in range(number + 1):
+            result += sum([i_num ** 2 for i_num in range(10000)])
+
+        return result
+
+
+@log_methods("b d Y - H:M:S")
+class B(A):
+    def test_sum_1(self):
+        super().test_sum_1()
+        print("Наследник test sum 1")
+
+    def test_sum_2(self):
+        print("test sum 2")
+        number = 200
+        result = 0
+        for _ in range(number + 1):
+            result += sum([i_num ** 2 for i_num in range(10000)])
+
+        return result
+
+
+my_obj = B()
+my_obj.test_sum_1()
+my_obj.test_sum_2()
+
--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = cls(*args, **kwargs)
+        return cls.instance
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
