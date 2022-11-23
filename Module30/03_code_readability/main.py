761ac47ef8fe5597edc52145658bf13759a10da6
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2022-11-23 12:23:02 +0300

    homework module 30 added.

--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+
+if __name__ == '__main__':
+    num_list = list(map(lambda num: num, range(0, 1001)))
+    print(num_list)
+
+    num_list_2 = [num for num in range(0, 1001)]
+    print(num_list_2)
